f5847472851cb7500a802e429c9de3dc90b567dc
Abraham Imohiosen <abrahamimohiosen@gmail.com>
2025-06-10 17:56:19 +0200

    Improve media loading error handling in XMedia component; log error and return placeholder rectangle when media is not loaded

--- editor/app/editor/_views/canvas/components/XMedia.tsx
+++ editor/app/editor/_views/canvas/components/XMedia.tsx
@@ -11,15 +11,20 @@ import Konva from "konva";
 // Component for rendering media images and videos
 type XMediaProps = Konva.NodeConfig & DMediaProps & { type: string };
 const XMedia = (props: XMediaProps) => {
-  
+
   const { image, videoElement, mediaLoaded, isVideo } = useMediaLoader({
     src: props.src,
     mediaType: props.type,
   });
 
   if (!mediaLoaded || !image) {
-    console.error("Media not loaded yet:", props?.src);
-    return null;
+    return <Rect
+      width={props.width || 100}
+      height={props.height || 100}
+      fill={"red"}
+      id={props.componentId}
+      {...props}
+    />;
   }
 
   return (
